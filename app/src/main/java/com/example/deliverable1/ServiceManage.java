287d64e691318ff536929c064ec69c798e8dec3a
Mikhail Kamara <kamaramikhail@gmail.com>
2021-11-01 19:24:26 -0400

    Dialog service manage implementation

--- app/src/main/java/com/example/deliverable1/ServiceManage.java
+++ app/src/main/java/com/example/deliverable1/ServiceManage.java
@@ -1,16 +1,24 @@
 package com.example.deliverable1;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,7 +26,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import service.Service;
 
 public class ServiceManage extends AppCompatActivity {
 
@@ -37,6 +48,15 @@ public class ServiceManage extends AppCompatActivity {
         list = (ListView) findViewById(R.id.serviceList);
 
         services=new ArrayList<>();
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                String idService = services.get(position);
+                showUpdateDeleteDialog(idService);
+                return false;
+            }
+        });
+
 
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -53,9 +73,58 @@ public class ServiceManage extends AppCompatActivity {
                         break;
                 }
                 store();
-                display();
+//                display();
             }
         });
+    }
+    private void showUpdateDeleteDialog(final String productId){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.activity_service_manage_dialog, null);
+        dialogBuilder.setView(dialogView);
+
+
+        final EditText editTextRate = (EditText) dialogView.findViewById(R.id.editNewRate);
+        final Button buttonUpdate = (Button) dialogView.findViewById(R.id.updateBtn);
+        final Button buttonDelete = (Button) dialogView.findViewById(R.id.deleteBtn);
+        final Button buttonCancel = (Button) dialogView.findViewById(R.id.cancelBtn);
+
+        DatabaseReference dr = database1.child(type).child(productId);
+        String name = dr.getKey();
+
+        dialogBuilder.setTitle(name);
+        final AlertDialog b = dialogBuilder.create();
+        b.show();
+
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double rate = Double.parseDouble(editTextRate.getText().toString());
+                if (!TextUtils.isEmpty(name)) {
+                    updateService(productId, rate);
+                    b.dismiss();
+                }
+            }
+        });
+
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteService(productId);
+                b.dismiss();
+            }
+        });
+    }
+
+    private void updateService(String id, double rate) {
+        DatabaseReference dr = database1.child(type).child(id);
+        dr.child("rate").setValue(rate);
+    }
+
+    private void deleteService(String id) {
+        DatabaseReference dr = database1.child(type).child(id);
+        dr.removeValue();
     }
 
     @Override
